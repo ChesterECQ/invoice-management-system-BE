e31ef2af9c467ff073abd713d162c665baf9b8e4
ChesterNg <chesterng@ecquaria.com>
2023-02-18 22:24:23 +0800

    initialize project

---
+++ src/main/kotlin/com/springKotlin/invoicemanagementsystem/InvoiceManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.springKotlin.invoicemanagementsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvoiceManagementSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvoiceManagementSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/springKotlin/invoicemanagementsystem/InvoiceManagementSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springKotlin.invoicemanagementsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvoiceManagementSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
