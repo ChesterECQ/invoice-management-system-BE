d7ed47006246d8e8f99800075cf56d66181d121d
ChesterNg <chesterng@ecquaria.com>
2023-02-19 18:13:13 +0800

    change sql statement regrading search

--- src/main/kotlin/com/springKotlin/invoicemanagementsystem/repository/InvoiceRepository.kt
+++ src/main/kotlin/com/springKotlin/invoicemanagementsystem/repository/InvoiceRepository.kt
@@ -11,7 +11,7 @@ interface InvoiceRepository : JpaRepository<Invoice, Long>{
     // Get all records from invoice table based on search value
     @Query("SELECT i FROM Invoice i "+
             "WHERE CONCAT(i.country, i.customerId, i.description, i.invoiceDate," +
-            " i.invoiceNo, i.quantity, i.stockCode, i.unitPrice) LIKE %:searchVal%")
+            " i.invoiceNo, i.quantity, i.stockCode, i.unitPrice)     LIKE %:searchVal%")
     fun findBySearchVal(searchVal: String) : List<Invoice>
 
 }
